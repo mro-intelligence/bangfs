b0242b9d4afc8e0b135052171dc4c587df962177
gosub2k <31038611+gosub2k@users.noreply.github.com>
2026-03-01 09:50:26 -0500

    updated bangfuse/metadata_utils.go

--- bangfuse/metadata_utils.go
+++ bangfuse/metadata_utils.go
@@ -28,8 +28,10 @@ func MetadataToFuseAttr(inum uint64, meta *pb.InodeMeta, out *fuse.Attr) {
 	out.Mtimensec = uint32(meta.MtimeNs % 1e9)
 	out.Ctime = uint64(meta.CtimeNs / 1e9)
 	out.Ctimensec = uint32(meta.CtimeNs % 1e9)
-	out.Blksize = GetChunkSize() // 100x speedup with Riak backend (ad-hoc test)
-	out.Blocks = uint64(len(meta.Chunks))
+	out.Blksize = GetChunkSize() // I/O hint for large reads
+
+	// st_blocks must be in 512-byte units (POSIX)
+	out.Blocks = (meta.Size + 511) / 512
 }
 
 // IsDir returns true if the metadata represents a directory.
