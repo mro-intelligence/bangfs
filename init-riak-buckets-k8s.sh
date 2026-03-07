5338388bb46a3c865bb280896678fc73c14eb123
gosub2k <31038611+gosub2k@users.noreply.github.com>
2026-03-06 19:37:10 -0500

    updated init-riak-buckets-k8s.sh

--- init-riak-buckets-k8s.sh
+++ init-riak-buckets-k8s.sh
@@ -6,21 +6,21 @@
 
 set -e
 
-BANGFS_NAMESPACE="${1:-test}"
-K8S_NAMESPACE="${2:-bangfs}"
-K8S_SERVICE="riak"
+if [[ -z "$BANGFS_NAMESPACE" ]]; then BANGFS_NAMESPACE="${1:-test}"; fi
+if [[ -z "$BANGFS_K8S_NAMESPACE" ]]; then BANGFS_K8S_NAMESPACE="${2:-bangfs}"; fi
+if [[ -z "$BANGFS_K8S_SERVICE" ]]; then BANGFS_K8S_SERVICE="riak"; fi
 
 echo "Initializing BangFS bucket types with prefix: $BANGFS_NAMESPACE"
-echo "Kubernetes namespace: $K8S_NAMESPACE"
-echo "Kubernetes service: $K8S_SERVICE"
+echo "Kubernetes namespace: $BANGFS_K8S_NAMESPACE"
+echo "Kubernetes service: $BANGFS_K8S_SERVICE"
 
 create_bucket_type() {
     local name="$1"
     local props="$2"
 
     echo " ==== Creating bucket type '$name'... ===="
-    kubectl exec -n "$K8S_NAMESPACE" -it svc/"$K8S_SERVICE" -- riak-admin bucket-type create "$name" "$props"
-    kubectl exec -n "$K8S_NAMESPACE" -it svc/"$K8S_SERVICE" -- riak-admin bucket-type activate "$name"
+    kubectl exec -n "$BANGFS_K8S_NAMESPACE" -it svc/"$BANGFS_K8S_SERVICE" -- riak-admin bucket-type create "$name" "$props"
+    kubectl exec -n "$BANGFS_K8S_NAMESPACE" -it svc/"$BANGFS_K8S_SERVICE" -- riak-admin bucket-type activate "$name"
 }
 
 # For single-node testing, use n_val=1, w=1, r=1
